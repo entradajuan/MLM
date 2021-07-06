ed819959fe072bc41d5db94ea8959e6ace523c18
entradajuan <entradajuan@yahoo.es>
2021-07-06 18:27:55 +0000

    new MLM model

--- mlm.py
+++ mlm.py
@@ -5,8 +5,6 @@
 !pip install git+https://github.com/huggingface/transformers
 !pip list | grep -E 'transformers|tokenizers'
 
-%%time
-
 from pathlib import Path
 from tokenizers import ByteLevelBPETokenizer
 
@@ -22,6 +20,37 @@ tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=
     "<unk>",
     "<mask>",
 ])
+
+from tokenizers.implementations import ByteLevelBPETokenizer
+from tokenizers.processors import BertProcessing
+
+tokenizer._tokenizer.post_processor = BertProcessing(
+    ("</s>", tokenizer.token_to_id("</s>")),
+    ("<s>", tokenizer.token_to_id("<s>")),
+)
+tokenizer.enable_truncation(max_length=512)
+
+!nvidia-smi
+
+import torch
+torch.cuda.is_available()
+
+from transformers import RobertaConfig
+
+config = RobertaConfig(
+    vocab_size=52_000,
+    max_position_embeddings=514,
+    num_attention_heads=12,
+    num_hidden_layers=6,
+    type_vocab_size=1,
+)
+
+print(config)
+
+from transformers import RobertaTokenizer
+tokenizer = RobertaTokenizer.from_pretrained("./KantaiBERT", max_length=512)
+
+
 
 
 
