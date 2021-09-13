c1d7a41c9a4efe3bc11b9037809f30c3c382ce50
entradajuan <entradajuan@yahoo.es>
2021-09-13 17:19:03 +0000

    TF2 solution

---
+++ tf_mlm.py
@@ -0,0 +1,5 @@
+!curl -L https://raw.githubusercontent.com/PacktPublishing/Transformers-for-Natural-Language-Processing/master/Chapter03/kant.txt --output "kant.txt"
+!curl -L https://www.gutenberg.org/cache/epub/4962/pg4962.txt --output "TheStoryOfGermLife.txt"
+!curl -L https://www.gutenberg.org/cache/epub/27713/pg27713.txt --output "BacteriologicalTechnique.txt"
+
+
