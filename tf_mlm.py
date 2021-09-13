68674b9a64cedc2c5f672432eb741abc0fd83112
entradajuan <entradajuan@yahoo.es>
2021-09-13 18:31:15 +0000

    TF2 solution

--- tf_mlm.py
+++ tf_mlm.py
@@ -2,39 +2,38 @@
 !curl -L https://www.gutenberg.org/cache/epub/4962/pg4962.txt --output "TheStoryOfGermLife.txt"
 !curl -L https://www.gutenberg.org/cache/epub/27713/pg27713.txt --output "BacteriologicalTechnique.txt"
 
-
+import tensorflow_text as tf_text
+import tensorflow as tf
 import pandas as pd
 
 with open('kant.txt') as f:
-    sentences = f.readlines()
+    lines = f.readlines()
 
-type(sentences)
 
-vocab = set()
-data = []
+#text_input = ["Madrid is the capital of Spain.",
+#              "I love to eat ice cream in Summer!!!",
+#              "What's your name?"]
+
 MAX_LEN = 0
-for sentence in sentences:
-  words_in_sentence = sentence[:-1].split(" ")
-  data.append(words_in_sentence)
-  for word in words_in_sentence:
-    vocab.add(word)
-  if MAX_LEN < len(sentence):
-    MAX_LEN = len(sentence)
+text_input = []
+for line in lines:
+  text_input.append(line) 
+  if MAX_LEN < len(line):
+    MAX_LEN = len(line)
 
-print(MAX_LEN)
-idx = 2560
-print(sentences[idx])
-print(data[idx])
+for sentence in text_input[:10]:
+  print(sentence)
 
+print(type(text_input))
+tokenizer = tf_text.WhitespaceTokenizer()
+tokenized = tokenizer.tokenize(text_input)
 
-# Tokenize
-word_2_idx = {w:i for i, w in enumerate(vocab)}
-idx_2_word = {i:w for i, w in enumerate(vocab)}
-print(word_2_idx["war"], ' is ', idx_2_word[word_2_idx["war"]])
-
-
+print(type(tokenized))
+print(tokenized[1])
 
 # Embedding
 EMBEDDING_DIM = 300
 VOCAB_SIZE = len(vocab)
+
+
 
