6424cfb9115d065c0657683ae296829df65814dc
entradajuan <entradajuan@yahoo.es>
2021-09-16 18:19:20 +0000

    creating seq2seq dataset

--- tf_mlm.py
+++ tf_mlm.py
@@ -2,34 +2,42 @@
 !curl -L https://www.gutenberg.org/cache/epub/4962/pg4962.txt --output "TheStoryOfGermLife.txt"
 !curl -L https://www.gutenberg.org/cache/epub/27713/pg27713.txt --output "BacteriologicalTechnique.txt"
 
-import tensorflow_text as tf_text
 import tensorflow as tf
 import pandas as pd
+import numpy as np
 
 with open('kant.txt') as f:
     lines = f.readlines()
 
+# Tokenizer
 
-#text_input = ["Madrid is the capital of Spain.",
-#              "I love to eat ice cream in Summer!!!",
-#              "What's your name?"]
+tokenizer = tf.keras.preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts(lines)
+sequences = tokenizer.texts_to_sequences(lines)
 
-MAX_LEN = 0
-text_input = []
-for line in lines:
-  text_input.append(line) 
-  if MAX_LEN < len(line):
-    MAX_LEN = len(line)
+print("Count of characters:",tokenizer.word_counts)
+print("Length of text:",tokenizer.document_count)
+print("Character index",tokenizer.word_index)
+print("Frequency of characters:",tokenizer.word_docs)
 
-for sentence in text_input[:10]:
-  print(sentence)
+sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, padding='post')
 
-print(type(text_input))
-tokenizer = tf_text.WhitespaceTokenizer()
-tokenized = tokenizer.tokenize(text_input)
+WINDOW_SIZE = 3
+def create_dataset(sentence):
+  sentence = sentence.tolist()
+  corpus = []
+  sentence.append(0)
+  sentence.append(0)
+  for i, word in enumerate(sentence):
+    words_set = sentence[i:i+WINDOW_SIZE]
+    if len(words_set)==WINDOW_SIZE:
+      if [0, 0, 0] != words_set: 
+        corpus.append(words_set)
+  print(type(corpus))
+  return corpus
 
-print(type(tokenized))
-print(tokenized[1])
+data = [create_dataset(sentence) for sentence in sequences]
+
 
 # Embedding
 EMBEDDING_DIM = 300
